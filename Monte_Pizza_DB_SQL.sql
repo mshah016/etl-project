20f1d03ff6cfe7636beac840e0825759f321ee4f
Monte Rodriguez <montejrodriguez@outlook.com>
2020-06-13 13:06:13 -0700

    Finalized SQL and Jupyter Notebook

--- Monte_Pizza_DB_SQL.sql
+++ Monte_Pizza_DB_SQL.sql
@@ -48,11 +48,14 @@ SELECT * FROM location_df
 SELECT * FROM price_range_df
 
 -- Join tables to create database and create view once complete.
+DROP VIEW final_pizza_db
+
 CREATE VIEW final_pizza_db AS
 SELECT ldf.location_id AS "Location ID" , rcdf.restaurant_name AS "Restaurant Name", 
 ldf.restaurant_address AS "Restaurant Address", rcdf.count AS "Rest. Chain Count", 
 ldf.latitude AS "Latitude", ldf.longitude AS "Longitude", ldf.city AS "City", 
-ldf.postal_code	as "Postal Code", country AS "Country"	
+ldf.postal_code	as "Postal Code", country AS "Country", 
+prdf.price_range_min AS "Min Price Range" , prdf.price_range_max AS "Max Price Range"
 FROM restaurant_chains_df AS rcdf
 JOIN location_df AS ldf ON
 ldf.restaurant_name = rcdf.restaurant_name
@@ -60,6 +63,8 @@ JOIN price_range_df AS prdf ON
 ldf.location_id = prdf.location_id
 
 -- Convert view to table. 
+DROP TABLE final_pizza_database
+
 CREATE TABLE final_pizza_database AS 
 SELECT * FROM final_pizza_db
 
